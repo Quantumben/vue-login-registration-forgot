fc2b2afa6584c6252477c79fe84f086789809a64
QuantumBen <omosabenedict@gmail.com>
2024-02-06 06:37:38 +0100

    Setting vue router4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from "pinia";
+import router from "./router";
 
-createApp(App).mount('#app')
+import "./axios";
+import "./style.css";
+import App from "./App.vue";
+
+
+
+const app = createApp(App);
+app.use(router);
+
+app.mount("#app");
